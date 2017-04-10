e02252887baae582dded1a9ecb7480622b9d633c
Marcus Ulmefors <developer@ulmefors.com>
2017-04-10 20:35:19 +0200

    feat: Calculate RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,24 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+	long nb_est = estimations.size();
+	assert(nb_est != 0);
+	assert(nb_est == ground_truth.size());
+
+	long nb_dim = estimations[0].size();
+	assert(nb_dim != 0);
+
+	VectorXd error = VectorXd::Zero(nb_dim);
+	VectorXd residual = VectorXd::Zero(nb_dim);
+
+	for (int i = 0; i < nb_est; i++) {
+		residual = estimations[i] - ground_truth[i];
+		residual = residual.array() * residual.array();
+		error += residual;
+	}
+
+	VectorXd rmse = sqrt(error/nb_est);
+
+	return rmse;
 }
