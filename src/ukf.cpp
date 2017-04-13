6c3bebf7d0c9a49e1ea5b0a1b58e50e1821f16a8
Marcus Ulmefors <developer@ulmefors.com>
2017-04-13 19:21:04 +0200

    feat: Tune acceleration noise

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -42,10 +42,10 @@ UKF::UKF() {
 	time_us_ = 0;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 0.10;
+  std_a_ = 0.25;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.60;
+  std_yawdd_ = 0.55;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
